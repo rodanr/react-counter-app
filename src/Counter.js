76537ded34aabad97d8d15153ead18be18c98d5d
Rodan Ramdam <rodan.ramdam@gmail.com>
2022-07-21 12:25:39 +0545

    work with state inside a class component

--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,35 @@
 import React, { Component } from "react";
 
 export default class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: props.initialCount,
+    };
+  }
+  changeCount(amount) {
+    this.setState((prevState) => {
+      return { count: prevState.count + amount };
+    });
+  }
   render() {
     return (
       <div>
-        <button>-</button>
-        <span>{this.props.initialCount}</span>
-        <button>+</button>
+        <button
+          onClick={() => {
+            this.changeCount(-1);
+          }}
+        >
+          -
+        </button>
+        <span>{this.state.count}</span>
+        <button
+          onClick={() => {
+            this.changeCount(1);
+          }}
+        >
+          +
+        </button>
       </div>
     );
   }
